043ffedbddaa0e9d07ee5df9d541597bd2653d9d
ycy-ycy <2923616405@qq.com>
2019-12-21 16:20:17 +0800

    Initial commit

---
+++ src/com/myCode/beanSet/Student.java
@@ -0,0 +1,38 @@
+//IntelliJ IDEA
+//Sprin01
+//Student
+//2019/12/21
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.myCode.beanSet;
+
+public class Student {
+    private String name;
+    private String age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/com/myCode/main/Manager.java
@@ -0,0 +1,26 @@
+//IntelliJ IDEA
+//Sprin01
+//Manager
+//2019/12/21
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.myCode.main;
+
+
+import com.myCode.beanSet.Student;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Manager {
+    public static void main(String[] args){
+        Resource resource = new ClassPathResource("applicationContext.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+        Student student = (Student) factory.getBean("student");
+        System.out.println("学生姓名："+student.getName());
+        System.out.println("学生年龄："+student.getAge());
+        System.out.println("学生性别："+student.getSex());
+    }
+}
